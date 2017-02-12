51b10a332cf338aa25b22e1687c34db00d84f730
Valentin GOT <gotvalentin@gmail.com>
2017-02-12 18:12:13 +0100

    chore(app): add hammerjs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,27 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
 import { CalendarComponent } from "angular2-fullcalendar/src/calendar/calendar";
-import { WorkCalendarComponent } from './work-calendar/work-calendar.component';
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
+import { WorkCalendarComponent } from './work-calendar/work-calendar.component';
+import { AddWorkDialog } from './work-calendar/shared/add-work.dialog';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CalendarComponent,
-    WorkCalendarComponent
-  ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     MaterialModule.forRoot()
+  ],
+  declarations: [
+    AppComponent,
+    CalendarComponent,
+    WorkCalendarComponent,
+    AddWorkDialog
+  ],
+  entryComponents: [
+    AddWorkDialog
   ],
   providers: [],
   bootstrap: [AppComponent]
