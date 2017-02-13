cc7920fc8bfa3e9699173eca127e0f88b45ccdb7
Valentin GOT <gotvalentin@gmail.com>
2017-02-13 23:10:40 +0100

    chore(work-calendar): call to events API

--- src/app/work-calendar/work-calendar.component.ts
+++ src/app/work-calendar/work-calendar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MdDialogRef, MdDialog } from '@angular/material';
 
 import { AddWorkDialog } from './shared/add-work-dialog/add-work.dialog';
+import { EventService } from '../shared/event/event.service';
 
 @Component({
   selector: 'wo-work-calendar',
@@ -11,92 +12,27 @@ import { AddWorkDialog } from './shared/add-work-dialog/add-work.dialog';
 export class WorkCalendarComponent implements OnInit {
   addWorkDialogRef: MdDialogRef<AddWorkDialog>;
 
-  calendarOptions: Object = {
-    locale: 'fr',
-    height: 'parent',
-    fixedWeekCount : false,
-    // defaultDate: '2016-09-12',
-    editable: false,
-    eventLimit: true, // allow "more" link when too many events
+  calendarOptions = {
+    locale            : 'fr',
+    height            : 'parent',
+    fixedWeekCount    : false,
+    editable          : false,
+    eventLimit        : true, // TODO Remove
     handleWindowResize: true,
-    dayClick: (date, jsEvent, view) => {
+    dayClick          : (date, jsEvent, view) => {
       this.addWorkDialogRef = this.dialog.open(AddWorkDialog);
       this.addWorkDialogRef.componentInstance.date = date;
     },
-    events: [
-      {
-        title: 'All Day Event',
-        start: '2016-09-01',
-        color: '#029AE4'
-      },
-      {
-        title: 'Long Event',
-        start: '2016-09-07',
-        end: '2016-09-10',
-        color: '#029AE4'
-      },
-      {
-        id: 999,
-        title: 'Repeating Event',
-        start: '2016-09-09T16:00:00',
-        color: '#029AE4'
-      },
-      {
-        id: 999,
-        title: 'Repeating Event',
-        start: '2016-09-16T16:00:00',
-        color: '#029AE4'
-      },
-      {
-        title: 'Conference',
-        start: '2016-09-11',
-        end: '2016-09-13',
-        color: '#029AE4'
-      },
-      {
-        title: 'Meeting',
-        start: '2016-09-12T10:30:00',
-        end: '2016-09-12T12:30:00',
-        color: '#029AE4'
-      },
-      {
-        title: 'Lunch',
-        start: '2016-09-12T12:00:00',
-        color: '#029AE4'
-      },
-      {
-        title: 'Meeting',
-        start: '2016-09-12T14:30:00',
-        color: '#029AE4'
-      },
-      {
-        title: 'Happy Hour',
-        start: '2016-09-12T17:30:00',
-        color: '#029AE4'
-      },
-      {
-        title: 'Dinner',
-        start: '2016-09-12T20:00:00',
-        color: '#029AE4'
-      },
-      {
-        title: 'Birthday Party',
-        start: '2016-09-13T07:00:00',
-        color: '#029AE4'
-      },
-      {
-        title: 'Click for Google',
-        url: 'http://google.com/',
-        start: '2016-09-28',
-        color: '#029AE4'
-      }
-    ]
+    events            : []
   };
 
   constructor(
-    private dialog: MdDialog
+    private dialog: MdDialog,
+    private eventService: EventService
   ) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.eventService.getAll().subscribe((events) => this.calendarOptions.events = events);
+  }
 
 }
