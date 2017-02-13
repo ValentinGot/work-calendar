abaea6041fc169f389a38ff2001bfb0eb52eacc1
Valentin GOT <gotvalentin@gmail.com>
2017-02-13 22:42:48 +0100

    chore(add-work-dialog): use Project service

--- src/app/work-calendar/shared/add-work-dialog/add-work.dialog.ts
+++ src/app/work-calendar/shared/add-work-dialog/add-work.dialog.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MdDialogRef } from '@angular/material';
+
+import { ProjectService } from '../../../shared/project/project.service';
+import { Project } from '../../../shared/project/project.model';
 
 @Component({
   selector: 'wo-add-work-dialog',
@@ -9,37 +12,39 @@ import { MdDialogRef } from '@angular/material';
 export class AddWorkDialog implements OnInit {
   date: any;
   dateString: string;
-  workTypes =   [
-    { id: 1, value: 'Projet' },
-    { id: 2, value: 'Congé' },
-    { id: 3, value: 'Arrêt maladie' }
-  ];
-  projects = [
-    { id: 1, value: '4129 - Croix-Rouge Française - GAIA Lot 2' },
-    { id: 2, value: '4036 - Télécom Santé - Patient Ambulatoire' },
-    { id: 3, value: '4299 - Oraneg - Module Vitrine' },
-    { id: 4, value: '4389 - ARKEA - Générateur' }
-  ];
+  projects: Project[];
+  workTypes: Array<{ id: number, value: string }>;
+
   selectedType: number;
   selectedProject: number;
-  dayTime = {
+  selectedDayTime = {
     am: false,
     pm: false
   };
 
   constructor(
+    private projectService: ProjectService,
     public dialogRef: MdDialogRef<AddWorkDialog>
   ) {}
 
   ngOnInit () {
+    this.projectService.getAll().subscribe((projects) => this.projects = projects);
+
     this.dateString = `${this.capFirst(this.date.format('dddd'))} ${this.date.format('DD')} ${this.capFirst(this.date.format('MMMM'))}`;
+    this.workTypes = [
+      { id: 1, value: 'Projet' },
+      { id: 2, value: 'Congé' },
+      { id: 3, value: 'Arrêt maladie' }
+    ];
+
+    // Pre-select work type (default on 'Project')
     this.selectedType = this.workTypes[0].id;
 
     // Pre-select day time depending on the current hour
     if ((new Date()).getHours() <= 12) {
-      this.dayTime.am = true;
+      this.selectedDayTime.am = true;
     } else {
-      this.dayTime.pm = true;
+      this.selectedDayTime.pm = true;
     }
   }
 
