f8eeee9e262f31d0a142ed4b89d2a469d1254439
Valentin GOT <gotvalentin@gmail.com>
2017-02-14 14:54:36 +0100

    feat(event): add an exists method

--- src/app/shared/event/event.service.ts
+++ src/app/shared/event/event.service.ts
@@ -8,6 +8,11 @@ import { Event } from './event.model';
 
 @Injectable()
 export class EventService extends HttpResponseHandler {
+  public DAY_TIME = {
+    AM: 1,
+    PM: 2
+  };
+
   private baseUrl: string = '/events';
   private url : string;
 
@@ -25,7 +30,28 @@ export class EventService extends HttpResponseHandler {
       .catch(this.handleError);
   }
 
-  post (event: Event): Observable<Event> {
+  public exists (day: string, dayTime: number): Observable<Boolean> {
+    let time = '';
+
+    switch (dayTime) {
+      case this.DAY_TIME.AM:
+        time = '09:00:00';
+        break;
+
+      case this.DAY_TIME.PM:
+        time = '14:00:00';
+        break;
+    }
+
+    return this.http.get(this.url, {
+      search: `start=${day}T${time}`
+    })
+      .map(this.extractData)
+      .map((events) => events.length >= 1)
+      .catch(this.handleError);
+  }
+
+  public post (event: Event): Observable<Event> {
     return this.http.post(this.url, event)
       .map(this.extractData)
       .catch(this.handleError);
