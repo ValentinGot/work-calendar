f752f24cfb1e76f41343c8987d19db91e14a0eb4
Valentin GOT <gotvalentin@gmail.com>
2017-02-21 21:15:29 +0100

    chore(work-calendar): add settings button

--- src/app/work-calendar/work-calendar.component.ts
+++ src/app/work-calendar/work-calendar.component.ts
@@ -6,6 +6,7 @@ import * as moment from 'moment';
 import { AddWorkDialog } from './shared/add-work-dialog/add-work.dialog';
 import { EventService } from '../shared/event/event.service';
 import { EventColors, Event } from '../shared/event/event.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'wo-work-calendar',
@@ -14,40 +15,19 @@ import { EventColors, Event } from '../shared/event/event.model';
 })
 export class WorkCalendarComponent implements OnInit {
   addWorkDialogRef: MdDialogRef<AddWorkDialog>;
+  calendarOptions;
 
   @ViewChild(CalendarComponent) myCalendar: CalendarComponent;
 
-  calendarOptions = {
-    locale: 'fr',
-    height: 'parent',
-    fixedWeekCount: false,
-    editable: false,
-    timeFormat: 'A',
-    buttonText: {
-      today: 'Auj.'
-    },
-    dayClick: (date) => {
-      this.addWorkDialogRef = this.dialog.open(AddWorkDialog);
-      this.addWorkDialogRef.componentInstance.date = date;
-
-      this.addWorkDialogRef.afterClosed().subscribe((event: Event) => {
-        if (event !== undefined) {
-          this.myCalendar.fullCalendar('renderEvent', this.addColor(event));
-        }
-      });
-    },
-    eventclick: (event) => {
-
-    },
-    events: []
-  };
-
   constructor(
     private dialog: MdDialog,
-    private eventService: EventService
+    private eventService: EventService,
+    private router: Router
   ) { }
 
   ngOnInit() {
+    this.calendarOptions = this.getCalendarOptions();
+
     this.eventService.getAll().subscribe((events) => this.calendarOptions.events = events.map((event) => this.addColor(event)));
   }
 
@@ -55,6 +35,49 @@ export class WorkCalendarComponent implements OnInit {
     event.color = (moment(event.start).format('A') === 'AM') ? EventColors.AM : EventColors.PM;
 
     return event;
+  }
+
+  getCalendarOptions () {
+    return {
+      locale        : 'fr',
+      height        : 'parent',
+      fixedWeekCount: false,
+      editable      : false,
+      timeFormat    : 'A',
+      customButtons : {
+        settings: {
+          text : 'Param.',
+          click: () => this.router.navigate([ '/settings' ])
+        }
+      },
+      header        : {
+        center: 'today,prev,next',
+        right : 'settings'
+      },
+      buttonIcons   : {
+        prev: '',
+        next: ''
+      },
+      buttonText    : {
+        today: 'Auj.',
+        prev : 'Prev.',
+        next : 'Suiv.'
+      },
+      dayClick      : (date) => {
+        this.addWorkDialogRef = this.dialog.open(AddWorkDialog);
+        this.addWorkDialogRef.componentInstance.date = date;
+
+        this.addWorkDialogRef.afterClosed().subscribe((event: Event) => {
+          if (event !== undefined) {
+            this.myCalendar.fullCalendar('renderEvent', this.addColor(event));
+          }
+        });
+      },
+      eventclick    : (event) => {
+
+      },
+      events        : []
+    };
   }
 
 }
