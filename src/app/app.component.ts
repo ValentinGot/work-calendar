3b78d45ab7ffd6efa1fa68ee6ba2f576bf5164c9
Valentin GOT <gotvalentin@gmail.com>
2017-02-21 20:13:08 +0100

    chore(app): add nedb

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as Datastore from 'nedb';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.scss' ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  ngOnInit () {
+    // let db = new Datastore({
+    //   filename: './storage.db',
+    //   autoload: true
+    // });
+
+    // db.insert({
+    //   msg: 'test'
+    // }, (err, newDoc) => {
+    //   console.log(err, newDoc);
+    // });
+
+    // db.find({}, (err, docs) => {
+    //   console.log(err, docs);
+    // });
+  }
+
 }
