4ee6e17184c9ae717a36063787fee8475f76f7df
Valentin GOT <gotvalentin@gmail.com>
2017-02-22 15:14:30 +0100

    chore(app): remove nedb sample

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import * as Datastore from 'nedb';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.scss' ]
 })
-export class AppComponent implements OnInit {
-
-  ngOnInit () {
-    // let db = new Datastore({
-    //   filename: './storage.db',
-    //   autoload: true
-    // });
-
-    // db.insert({
-    //   msg: 'test'
-    // }, (err, newDoc) => {
-    //   console.log(err, newDoc);
-    // });
-
-    // db.find({}, (err, docs) => {
-    //   console.log(err, docs);
-    // });
-  }
-
-}
+export class AppComponent { }
