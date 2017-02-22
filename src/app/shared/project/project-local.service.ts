4254815f1d52d7c458b8ddca3edf2bf0d808a0e2
Valentin GOT <gotvalentin@gmail.com>
2017-02-22 16:26:16 +0100

    feat(project): add local get & create methods

--- src/app/shared/project/project-local.service.ts
+++ src/app/shared/project/project-local.service.ts
@@ -30,4 +30,41 @@ export class ProjectLocalService implements ProjectInterface {
     });
   }
 
+  public get (id: number): Observable<Project> {
+    return Observable.create((observer) => {
+      this.projects.find({ _id: id }, (err, project) => {
+        if (err) {
+          observer.throw(err);
+        }
+
+        observer.next(project);
+        observer.complete();
+      });
+    });
+  }
+
+  public create (project: Project): Observable<Project> {
+    return Observable.create((observer) => {
+      this.projects.insert(project, (err, created) => {
+        if (err) {
+          observer.throw(err);
+        }
+
+        observer.next(created);
+        observer.complete();
+      });
+    });
+  }
+
+  public update (id: string, project: Project): Observable<Project> {
+    return Observable.create((observer) => {
+      observer.complete();
+    });
+  }
+
+  public remove (id: string): Observable<void> {
+    return Observable.create((observer) => {
+      observer.complete();
+    });
+  }
 }
