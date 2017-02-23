0f46b524e31728894f30d541ccd77bc046df5360
Valentin GOT <gotvalentin@gmail.com>
2017-02-23 10:45:56 +0100

    feat(add-imputation): create a quick imputation

--- src/app/work-calendar/shared/add-quick-imputation/add-quick-imputation.component.ts
+++ src/app/work-calendar/shared/add-quick-imputation/add-quick-imputation.component.ts
@@ -1,9 +1,13 @@
-import { Component, OnInit, Inject, EventEmitter, ViewChild } from '@angular/core';
+import { Component, OnInit, Inject, EventEmitter, ViewChild, Input } from '@angular/core';
 import { FormGroup, FormBuilder, NgForm, Validators } from '@angular/forms';
+import { MdDialogRef } from '@angular/material';
 
 import { ProjectInterface } from '../../../shared/project/project.interface';
 import { ProjectService } from '../../../shared/project/project.service';
 import { Project } from '../../../shared/project/project.model';
+import { EventService } from '../../../shared/event/event.service';
+import { Event, DayTime } from '../../../shared/event/event.model';
+import { AddImputationDialog } from '../add-imputation/add-imputation.dialog';
 
 @Component({
   selector: 'wo-add-quick-imputation',
@@ -11,6 +15,9 @@ import { Project } from '../../../shared/project/project.model';
   styleUrls: ['./add-quick-imputation.component.scss']
 })
 export class AddQuickImputationComponent implements OnInit {
+  @Input() date: any;
+  @Input() dialogRef: MdDialogRef<AddImputationDialog>;
+
   form: FormGroup;
   projects: Project[];
   submitted: boolean;
@@ -21,6 +28,7 @@ export class AddQuickImputationComponent implements OnInit {
 
   constructor(
     @Inject(ProjectService) private projectService: ProjectInterface,
+    private eventService: EventService,
     private formBuilder: FormBuilder
   ) {
     this.$onSubmit = new EventEmitter<boolean>();
@@ -32,7 +40,8 @@ export class AddQuickImputationComponent implements OnInit {
     this.form = this.formBuilder.group({
       project: [ null, Validators.required ],
       am     : (new Date()).getHours() <= 12,
-      pm     : (new Date()).getHours() > 12
+      pm     : (new Date()).getHours() > 12,
+      comment: ''
     });
 
     this.$onSubmit.subscribe(() => this.quickImputationForm.ngSubmit.emit());
@@ -42,9 +51,18 @@ export class AddQuickImputationComponent implements OnInit {
     this.submitted = true;
 
     if (this.form.valid && (this.form.value.am || this.form.value.pm)) {
-      console.log(this.form.value);
+      let events: Event[] = [];
+
+      if (this.form.value.am) {
+        events.push(this.eventService.make(this.date, DayTime.AM, this.form.value.project as Project, this.form.value.comment));
+      }
+      if (this.form.value.pm) {
+        events.push(this.eventService.make(this.date, DayTime.PM, this.form.value.project as Project, this.form.value.comment));
+      }
 
       this.submitted = false;
+
+      this.eventService.create(...events).subscribe((events) => this.dialogRef.close(events));
     }
   }
 
