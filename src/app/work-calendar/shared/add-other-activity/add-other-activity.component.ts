c9f12492d176da79b762304eb216b4d4ad0d2192
Valentin GOT <gotvalentin@gmail.com>
2017-02-23 17:50:11 +0100

    feat(add-other-activity): list default activities

--- src/app/work-calendar/shared/add-other-activity/add-other-activity.component.ts
+++ src/app/work-calendar/shared/add-other-activity/add-other-activity.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { MdDialogRef } from '@angular/material';
 
 import { AddImputation } from '../add-imputation/add-imputation.class';
 import { AddImputationDialog } from '../add-imputation/add-imputation.dialog';
+import { FormGroup, NgForm, FormBuilder, Validators } from '@angular/forms';
+import { Activity } from '../../../shared/activity/activity.model';
+import { ActivityService } from '../../../shared/activity/activity.service';
 
 @Component({
   selector: 'wo-add-other-activity',
@@ -13,11 +16,35 @@ export class AddOtherActivityComponent extends AddImputation implements OnInit {
   @Input() date: any;
   @Input() dialogRef: MdDialogRef<AddImputationDialog>;
 
-  constructor(
+  activities: Activity[];
+  form: FormGroup;
+  submitted: boolean;
 
+  @ViewChild('otherActivityForm') otherActivityForm: NgForm;
+
+  constructor(
+    private activityService: ActivityService,
+    private formBuilder: FormBuilder
   ) { super(); }
 
   ngOnInit() {
+    this.activityService.getAll().subscribe((activities) => this.activities = activities);
+
+    this.form = this.formBuilder.group({
+      activity: [ null, Validators.required ]
+    });
+
+    this.$onSubmit.subscribe(() => this.otherActivityForm.ngSubmit.emit());
+  }
+
+  onSubmit () {
+    this.submitted = true;
+
+    if (this.form.valid) {
+      console.log(this.form.value);
+
+      this.submitted = false;
+    }
   }
 
 }
