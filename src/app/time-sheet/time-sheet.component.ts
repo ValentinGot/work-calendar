388361af47e28c6e479c392d79dd1a52388f288d
Olivier LE LAY <liollury@gmail.com>
2017-02-24 16:32:11 +0100

    feat(time-sheet): show activities

--- src/app/time-sheet/time-sheet.component.ts
+++ src/app/time-sheet/time-sheet.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as moment from 'moment';
+import { ImputationService } from '../shared/imputation/imputation.service';
+import {Imputation, ImputationData, ImputationType} from "../shared/imputation/imputation.model";
 
 @Component({
   selector: 'wo-time-sheet',
@@ -8,19 +10,26 @@ import * as moment from 'moment';
 })
 export class TimeSheetComponent implements OnInit {
   timeSheet: any;
+  activities: Array<{
+    type: ImputationType,
+    data: Object
+  }>;
+  imputations: Array<Imputation>;
 
-  constructor() { }
+  constructor(private imputationService: ImputationService) { }
 
   ngOnInit() {
+    let month = moment();
     this.timeSheet = {
-      dayInMonth : this.dayInMonth()
+      dayInMonth : this.dayInMonth(month)
     }
+    this.getImputation(month);
   }
 
-  dayInMonth () {
+  dayInMonth (month: moment.Moment) {
     let dayInMonth = [];
-    let baseDate = moment("2017-02", "YYYY-MM");
-    for (let i = 0; i < moment("2017-02", "YYYY-MM").daysInMonth(); i++) {
+    let baseDate = moment(month).startOf('month');
+    for (let i = 0; i <  moment(month).startOf('month').daysInMonth(); i++) {
       baseDate.add(1, 'day');
       if (baseDate.day() > 1) {
         dayInMonth.push({
@@ -30,6 +39,22 @@ export class TimeSheetComponent implements OnInit {
       }
     }
     return dayInMonth;
+  }
+
+  getImputation (month: moment.Moment) {
+    this.imputationService.getAllRange(moment(month).startOf('month'), moment(month).endOf('month')).subscribe((imputations: Array<Imputation>) => {
+      this.imputations = imputations;
+      this.activities = [];
+      while(imputations.length > 0) {
+        let activity = imputations[0].data;
+        this.activities.push({
+          type: imputations[0].type,
+          data: activity
+        });
+        imputations = imputations.filter((imputation) => (<ImputationData>imputation.data)._id !== (<ImputationData>activity)._id);
+      }
+      console.log(this.activities);
+    });
   }
 
 }
